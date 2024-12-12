50d8e455dd1ddae0578b77b1a5e2ac4d8bd99ac9
fallennsyahid <umarusyahidmasudi1@gmail.com>
2024-12-13 05:05:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use PHPUnit\Event\TestSuite\Loaded;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/home', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Umaru Syahid M'], ['title' => 'About Page']);
+});
+
+Route::get('/posts', function () {
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+
+    // $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)->withQueryString()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
+
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
+
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
